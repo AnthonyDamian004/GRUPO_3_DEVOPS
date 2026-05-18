0199f561ca21b3a6cb67cf1663aeb1a2c4fccb22
AnthonyDamian004 <anthonydamian2209@gmail.com>
2026-05-18 18:26:15 -0500

    Refactor UserController for improved readability

--- src/main/java/com/cat/user/service/controller/UserController.java
+++ src/main/java/com/cat/user/service/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.cat.user.service.controller;
 
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.cat.user.service.dto.UserRequest;
 import com.cat.user.service.dto.UserResponse;
 import com.cat.user.service.service.UserService;
+
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,15 +20,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @PostMapping
-    public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(request));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> findAll() {
-        return ResponseEntity.ok(userService.findAll());
-    }
+	@PostMapping
+	public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRequest request) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(request));
+	}
 }
